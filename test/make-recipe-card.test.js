a2901ad60863b57289e0e35a9fe7b35d9b2124fd
Laura Badgley <l.badgleyy@gmail.com>
2019-03-13 14:07:20 -0700

    basic styling for hard coded cards

---
+++ test/make-recipe-card.test.js
@@ -0,0 +1,12 @@
+const test = QUnit.test;
+QUnit.module('recipe card template');
+
+test('make recipe card', assert => {
+    //arrange
+    const expected = /*html*
+    //act
+
+    //assert
+
+
+})
