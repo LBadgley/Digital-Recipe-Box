cdcf291322cd4c5c60e08573386277d1ea10af43
Laura Badgley <l.badgleyy@gmail.com>
2019-03-13 14:53:19 -0700

    RE-RESTRUCTED test

--- test/make-recipe-card.test.js
+++ test/make-recipe-card.test.js
@@ -20,10 +20,12 @@ test('make recipe card', assert => {
     //arrange
     const expected = /*html*/`
         <li>
-            <p>Chicken Couscous</p>
+            <p><a href="https://www.bbcgoodfood.com/recipes/13139/onepan-chicken-couscous">Chicken Couscous</a></p>
             <img src="https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg" alt="image of Chicken Couscous">
-            <p>Meal Id: 52850</p>
-        </li>`;
+            <p>Cuisine: Moroccan</p>
+            <p><a href="https://www.youtube.com/watch?v=GZQGy9oscVk" target="_blank">Video</a></p>
+        </li>
+        `;
     //act
     const result = makeRecipeCard(meal);
     //assert
