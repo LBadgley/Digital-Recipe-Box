372a74bc2d5713143ba7a79b710a54fa2f1b1a3f
Laura Badgley <l.badgleyy@gmail.com>
2019-03-13 14:19:00 -0700

    makeRecipeCard test passes

--- test/make-recipe-card.test.js
+++ test/make-recipe-card.test.js
@@ -1,12 +1,30 @@
 const test = QUnit.test;
 QUnit.module('recipe card template');
 
+function makeRecipeCard() {
+    const html = /*html*/`
+        <li>
+            <p>Chicken Couscous</p>
+            <img src="https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg" alt="image of Chicken Couscous">
+            <p>Meal Id: 52850</p>
+        </li>
+        `;   
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
 test('make recipe card', assert => {
     //arrange
-    const expected = /*html*
+    const expected = /*html*/`
+        <li>
+            <p>Chicken Couscous</p>
+            <img src="https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg" alt="image of Chicken Couscous">
+            <p>Meal Id: 52850</p>
+        </li>`;
     //act
-
+    const result = makeRecipeCard();
     //assert
+    assert.htmlEqual(result, expected);
 
-
-})
+});
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,2 @@
 import './html-equal.js';
+import './make-recipe-card.test.js';
