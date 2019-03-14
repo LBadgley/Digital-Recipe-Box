8081a6dbcc082af92bbc7320a7a1575d7677e7dd
Laura Badgley <l.badgleyy@gmail.com>
2019-03-14 14:48:45 -0700

    test for empty string search passes

--- src/make-search-meal-url.js
+++ src/make-search-meal-url.js
@@ -6,6 +6,9 @@ const MEAL_SEARCH_URL = `${BASE_URL}${API_KEY}`;
 // const MEAL_SEARCH_URL = 'https://www.food2fork.com/api/search?key=3029508e03964e2df91e90df762c09cd'
 
 export default function makeSearchMealUrl(queryOptions) {
+    if(!queryOptions.q) {
+        return '';
+    }
     const url = new URL(MEAL_SEARCH_URL);
     url.searchParams.set('q', queryOptions.q);
     url.searchParams.set('page', queryOptions.page);
--- test/make-search-meal-url.test.js
+++ test/make-search-meal-url.test.js
@@ -4,7 +4,8 @@ QUnit.module('make URL for fetch');
 
 test('include query and page', assert => {
     // arrange
-    const expected = 'https://www.food2fork.com/api/search?key=3029508e03964e2df91e90df762c09cd&q=beef&page=1';
+    // const expected = 'https://www.food2fork.com/api/search?key=3029508e03964e2df91e90df762c09cd&q=beef&page=1';
+    const expected = 'https://www.food2fork.com/api/search?key=63f112ca1f523330536f1178355e3f07&q=beef&page=1';
     const queryOptions = {
         q: 'beef',
         page: 1
@@ -13,6 +14,19 @@ test('include query and page', assert => {
     // act
     const result = makeSearchMealUrl(queryOptions);
 
+    // assert
+    assert.equal(result, expected);
+});
+
+test('return empty string if no search term', assert => {
+    // arrange
+    const expected = '';
+    const queryOptions = {
+        q: '',
+        page: 3
+    };
+    // act
+    const result = makeSearchMealUrl(queryOptions);
     // assert
     assert.equal(result, expected);
 });
