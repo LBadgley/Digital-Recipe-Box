cca4d0231ea9b4c48d88692687a531cb00542ed7
Laura Badgley <l.badgleyy@gmail.com>
2019-03-14 08:19:52 -0700

    micro-commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import meals from '../data/meals.js';
 import renderMealCards from './meal-card-components.js';
-import loadSearch from './search-component.js';
-
-loadSearch();
+import './search-component.js';
+// loadSearch();
 renderMealCards(meals);
 
 // fetch('https://www.food2fork.com/api/search?key=3029508e03964e2df91e90df762c09cd&q=chicken%20breast&page=2')
--- src/search-component.js
+++ src/search-component.js
@@ -1,10 +1,22 @@
+import { writeSearchToQuery } from './hash-query.js';
 const searchForm = document.getElementById('search-form');
 
-export default function loadSearch() {
-    searchForm.addEventListener('submit', event => {
-        event.preventDefault();
-        const formData = new FormData(searchForm);
-        const mainIngredient = formData.get('main-ingredient');
-        console.log('click', mainIngredient);
-    });
-}
+// export default function loadSearch() {
+    //         const formData = new FormData(searchForm);
+    //         const mainIngredient = formData.get('main-ingredient');
+    //         console.log('click', mainIngredient);
+    //     });
+    
+    
+    // }
+    
+
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(searchForm);
+    const mainIngredient = formData.get('main-ingredient');
+    const existingQuery = window.location.hash.slice(1);
+    const newQuery = writeSearchToQuery(existingQuery, mainIngredient);
+    window.location.hash = newQuery;
+});
+
