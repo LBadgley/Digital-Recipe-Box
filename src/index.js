955a3b4bd3307c423bb700f96978617bbf3f53bf
Laura Badgley <l.badgleyy@gmail.com>
2019-03-14 10:47:56 -0700

    FETCH IS SO HAPPENING

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import meals from '../data/meals.js';
+// import meals from '../data/meals.js';
 import renderMealCards from './meal-card-components.js';
 import { updateMainIngredient } from './search-component.js';
 import './search-component.js';
 import './paging-component.js';
 import { readFromQuery } from './hash-query.js';
-import makeSearchMealUrl from './make-search-meal-url.js'
+import makeSearchMealUrl from './make-search-meal-url.js';
 
-renderMealCards(meals);
 
 window.addEventListener('hashchange', () => {
     const existingQuery = window.location.hash.slice(1);
     const queryOptions = readFromQuery(existingQuery);
     updateMainIngredient(queryOptions.q);
-    console.log(queryOptions);
+    const url = makeSearchMealUrl(queryOptions);
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(result => result.recipes)
+    //     .then(meals => {
+    //         renderMealCards(meals);
+    //     });
 });
-// fetch('https://www.food2fork.com/api/search?key=3029508e03964e2df91e90df762c09cd&q=chicken%20breast&page=2')
-//     .then(response => response.json())
-//     .then(body => console.log(body));
-    
