a99184b6d2c69401e21f32a301a0bc41db89bc12
Laura Badgley <l.badgleyy@gmail.com>
2019-03-14 08:34:23 -0700

    eventListener in index.js logs changes in hash

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import meals from '../data/meals.js';
 import renderMealCards from './meal-card-components.js';
 import './search-component.js';
 import './paging-component.js';
+import { readFromQuery } from './hash-query.js';
 
-// loadSearch();
 renderMealCards(meals);
 
+window.addEventListener('hashchange', () => {
+    const existingQuery = window.location.hash.slice(1);
+    const queryOptions = readFromQuery(existingQuery);
+    console.log(queryOptions);
+});
 // fetch('https://www.food2fork.com/api/search?key=3029508e03964e2df91e90df762c09cd&q=chicken%20breast&page=2')
 //     .then(response => response.json())
 //     .then(body => console.log(body));
