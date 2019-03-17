7c7b9dca966861562fd0ef941eae6f16857353d1
Laura Badgley <l.badgleyy@gmail.com>
2019-03-17 11:10:01 -0700

    added favorite star- not styled

--- src/meal-card-components.js
+++ src/meal-card-components.js
@@ -4,6 +4,7 @@ import { auth, favoritesByUserRef } from '../src/firebase.js';
 export function makeRecipeCard(meal) {
     const html = /*html*/`
         <li>
+            <span class="favorite-star">☆</span>
             <a href=${meal.f2f_url} target="_blank">${meal.title}</a>
             <img src=${meal.image_url} alt="image of ${meal.title}">
             <a href=${meal.publisher_url} target="_blank">${meal.publisher}</a>
@@ -14,16 +15,28 @@ export function makeRecipeCard(meal) {
     return template.content;
 }
 
+const recipeBox = document.getElementById('recipe-box');
+
 export default function renderMealCards(meals) {
     clearCards();
     meals.forEach(meal => {
         const dom = makeRecipeCard(meal);
         recipeBox.appendChild(dom);
-        // const favorite
+        const favoriteStar = dom.querySelector('.favorite-star');
+        favoriteStar.addEventListener('click', () => {
+            const userId = auth.currentUser.uid;
+            const userFavoritesRef = favoritesByUserRef.child(userId);
+            const userFavoriteRecipeRef = userFavoritesRef.child(meal.count);
+            userFavoriteRecipeRef.set({
+                id: meal.count,
+                title: meal.title,
+                image: meal.image_url,
+                publisher: meal.publisher_url
+            });
+        });
+        recipeBox.appendChild(dom);
     });
 }
-
-const recipeBox = document.getElementById('recipe-box');
 
 function clearCards() {
     while(recipeBox.children.length > 0) {
